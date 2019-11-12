e03034c6be0b8e3b63047635304db35843c233ab
Juan Siverio <alu0100585704@ull.edu.es>
2019-11-12 19:23:14 +0000

    Primeras pruebas y expectativa para  Debe existir nombre de Alimento

--- lib/p6.rb
+++ lib/p6.rb
@@ -3,4 +3,17 @@ require "p6/version"
 module P6
   class Error < StandardError; end
   # Your code goes here...
+  class Alimentos
+     attr_accessor :nombre
+
+     #tabla de alimentos composición por kg e impacto ambiental
+     #Nombre - proteinas - cabo hidratos - lipidos- GEI - Terreno m2 año
+
+     @@tabla = {'Carne de vaca' => [21,1,0.0,3.1,50.0,164.0],'Carne de cordero' => [18.0,0.0,17.0,20.0,185.0],'Camarones(piscifactoria)' => [17.6,1.5,0.6,18.0,2.0],'Chocolate' =>[5.3,47.0,40.0,2.3,3.4]}
+    def  initialize(nombre)
+        @nombre = nombre
+        
+    end
+  end
+
 end
--- spec/p6_spec.rb
+++ spec/p6_spec.rb
@@ -1,9 +1,18 @@
+require "lib/p6"
 RSpec.describe P6 do
   it "has a version number" do
     expect(P6::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe P6::Alimentos do
+    before (:each) do
+        @alimento = P6::Alimentos.new("Carne de vaca")
+    end
+    context "Comprobacion de variables de instancia" do
+      it "Nombre de alimento" do
+        expect(@alimento.nombre).not_to be nil
+      end
+    
+    end
   end
 end
