65433d5901075f1df4aa02fd40875bfe9d27fdbb
Juan Siverio <alu0100585704@ull.edu.es>
2019-11-12 19:45:08 +0000

    Comprobacion de variables de instancia hecha

--- lib/p6.rb
+++ lib/p6.rb
@@ -4,15 +4,21 @@ module P6
   class Error < StandardError; end
   # Your code goes here...
   class Alimentos
-     attr_accessor :nombre
+     attr_accessor :nombre, :kgCO, :terreno
+     
 
      #tabla de alimentos composición por kg e impacto ambiental
      #Nombre - proteinas - cabo hidratos - lipidos- GEI - Terreno m2 año
 
-     @@tabla = {'Carne de vaca' => [21,1,0.0,3.1,50.0,164.0],'Carne de cordero' => [18.0,0.0,17.0,20.0,185.0],'Camarones(piscifactoria)' => [17.6,1.5,0.6,18.0,2.0],'Chocolate' =>[5.3,47.0,40.0,2.3,3.4]}
+     @@tabla = {'Carne de vaca' => [21.1,0.0,3.1,50.0,164.0],'Carne de cordero' => [18.0,0.0,17.0,20.0,185.0],'Camarones(piscifactoria)' => [17.6,1.5,0.6,18.0,2.0],'Chocolate' =>[5.3,47.0,40.0,2.3,3.4]}
     def  initialize(nombre)
-        @nombre = nombre
-        
+      puts 
+       if @@tabla[nombre]  !=  nil
+          @nombre = nombre
+          @kgCO = @@tabla[nombre][3]
+          @terreno = @@tabla[nombre][4]
+          
+       end
     end
   end
 
--- spec/p6_spec.rb
+++ spec/p6_spec.rb
@@ -5,14 +5,22 @@ RSpec.describe P6 do
   end
 
   describe P6::Alimentos do
-    before (:each) do
+    before (:all) do
         @alimento = P6::Alimentos.new("Carne de vaca")
     end
     context "Comprobacion de variables de instancia" do
       it "Nombre de alimento" do
-        expect(@alimento.nombre).not_to be nil
+        expect(@alimento.nombre).not_to be nil        
       end
-    
+
+      it "kgCO2 eq" do
+        expect(@alimento.kgCO).not_to be nil        
+      end
+      it "Terreno empleado" do
+        expect(@alimento.terreno).not_to be nil        
+      end
+
+      
     end
   end
 end
